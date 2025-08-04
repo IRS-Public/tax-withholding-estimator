b48b0ff718e437518e7daadccf26df9a17e333ab
Ronak Patel <84742904+ronaktruss@users.noreply.github.com>
2025-08-04 13:08:16 -0400

    Add support for multiple fact files (#23)
    * Add support for multiple fact files
    
    * Rename facts.xml

--- src/main/scala/gov/irs/twe/main.scala
+++ src/main/scala/gov/irs/twe/main.scala
@@ -3,16 +3,32 @@ package gov.irs.twe
 import gov.irs.twe.generators.Website
 
 import scala.io.Source
+import java.io.File
+import scala.xml.NodeBuffer
 
 def main(args: Array[String]): Unit = {
+  // Processing for handling multiple xml files for facts
+  val factDirectoryPath = os.pwd / "src" / "main" / "resources" / "twe" / "facts"
+  val factsDirectory = new File(factDirectoryPath.toString)
+  val listOfFiles = if (factsDirectory.exists && factsDirectory.isDirectory) {
+    factsDirectory.listFiles.filter(_.isFile).filter(_.getName.endsWith(".xml")).toList
+  } else {
+    List.empty[File]
+  }
+
+  val facts = new NodeBuffer()
+  for (file <- listOfFiles) {
+    val fileName = file.getName()
+    val factsFile = Source.fromResource(s"twe/facts/$fileName").getLines().mkString("\n")
+    val factDictionary = xml.XML.loadString(factsFile)
+    val factNodes = factDictionary \ "Facts" \ "_"
+    facts ++= factNodes
+  }
+  val allFacts = <FactDictionaryModule><Facts>{facts}</Facts></FactDictionaryModule>
 
   val flowFile = Source.fromResource("twe/flow.xml").getLines().mkString("\n")
-  val factsFile = Source.fromResource("twe/facts.xml").getLines().mkString("\n")
-
   val flow = xml.XML.loadString(flowFile)
-  val facts = xml.XML.loadString(factsFile)
-
-  val site = Website.fromXmlConfig(flow, facts)
+  val site = Website.fromXmlConfig(flow, allFacts)
 
   // Delete out/ directory and add files to it
   val outDir = os.pwd / "out"
