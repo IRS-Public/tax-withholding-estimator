b9d8e3ab77e265bc59ff1f730c7bce2c75966287
Johan Ortega-Rios <179751776+jaortegarios@users.noreply.github.com>
2025-08-20 09:28:12 -0500

    Add calculations for week-based remaining pay periods (#160)
    * Bringing in weekly frequency calculation facts from prototype repo
    
    * Updated fact naming and added descriptions. Added unit tests for tentativeRemainingPayPeriods and remainingPayPeriods
    
    * Implemented a fractional remaining payment periods fact for any income calculations, added unit tests for verification
    
    * Removed redundant fact graph save from pay period calc spec
    
    * Removed test fact
    
    * Fixed reference to renamed fact that was missed in merge conflict
    
    * Ran formatter on updated files

---
+++ src/test/scala/gov/irs/twe/factDictionary/PayPeriodCalculationsSpec.scala
@@ -0,0 +1,164 @@
+package gov.irs.twe.factDictionary
+
+import gov.irs.factgraph.types.Collection
+import gov.irs.factgraph.types.Day
+import gov.irs.factgraph.types.Dollar
+import gov.irs.factgraph.types.Enum
+import gov.irs.factgraph.types.Rational
+import gov.irs.factgraph.FactDictionaryForTests
+import gov.irs.factgraph.Path
+import gov.irs.twe.FileLoaderHelper
+import java.util.UUID
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.GivenWhenThen
+
+class PayPeriodCalculationsSpec extends AnyFunSuite with GivenWhenThen with TableDrivenPropertyChecks {
+  val factDictionary = setupFactDictionary()
+  val dummyUUID = "9ba9d216-81a8-4944-81ac-9410b2fad150" // Previously used as a W2 UUID in cdcc tests
+  val uuidVector = Vector(UUID.fromString(dummyUUID));
+
+  val jobs = Path("/jobs")
+  val payFrequency = Path(s"/jobs/#$dummyUUID/payFrequency")
+  val startDate = Path(s"/jobs/#$dummyUUID/startDate")
+  val endDate = Path(s"/jobs/#$dummyUUID/endDate")
+  val recentPayPeriodEnd = Path(s"/jobs/#$dummyUUID/mostRecentPayPeriodEnd")
+  val recentPayDate = Path(s"/jobs/#$dummyUUID/mostRecentPayDate")
+  val tentativeRemainingPayPeriods = Path(s"/jobs/#$dummyUUID/tentativeRemainingPayPeriods")
+  val remainingPayPeriods = Path(s"/jobs/#$dummyUUID/remainingPayPeriods")
+  val fractionalRemainingPayPeriods = Path(s"/jobs/#$dummyUUID/fractionalRemainingPayPeriods")
+  val ordinalEffectiveEndDate = Path(s"/jobs/#$dummyUUID/ordinalEffectiveEndDate")
+
+  // TODO! Add cases where the most recent pay date is before the start date
+
+  val weekly = Enum("weekly", "/payFrequencyOptions");
+  val biWeekly = Enum("biWeekly", "/payFrequencyOptions");
+  val dataTable = Table(
+    (
+      "frequencyValue",
+      "startDateValue",
+      "endDateValue",
+      "recentPayPeriodEndValue",
+      "recentPayDateValue",
+      "expected tentative pay periods",
+      "expected true pay periods",
+      "expected fractional pay periods",
+      "expected effective end date",
+      "description",
+    ),
+    // Weekly Pay Periods
+    (
+      weekly,
+      "2025-01-01",
+      "2025-10-25",
+      "2025-07-26",
+      "2025-08-01",
+      13,
+      13,
+      Rational(91, 7),
+      298,
+      "working for a portion of the year with no eoy overlap on weekly pay",
+    ),
+    (
+      weekly,
+      "2025-02-01",
+      "2025-12-31",
+      "2025-02-14",
+      "2025-02-18",
+      46,
+      45,
+      Rational(315, 7),
+      360,
+      "working for a portion of the year with eoy overlap on weekly pay",
+    ),
+    (
+      weekly,
+      "2025-01-01",
+      "2025-12-31",
+      "2025-07-26",
+      "2025-08-01",
+      23,
+      21,
+      Rational(147, 7),
+      354,
+      "working for the whole year with eoy overlap on weekly pay",
+    ),
+    // Biweekly Pay Periods
+    (
+      biWeekly,
+      "2025-01-01",
+      "2025-10-25",
+      "2025-07-26",
+      "2025-08-01",
+      7,
+      7,
+      Rational(91, 14),
+      298,
+      "working for a portion of the year with no eoy overlap on biweekly pay",
+    ),
+    (
+      biWeekly,
+      "2025-02-01",
+      "2025-12-31",
+      "2025-02-10",
+      "2025-02-14",
+      24,
+      22,
+      Rational(308, 14),
+      349,
+      "working for a portion of the year with eoy overlap on biweekly pay",
+    ),
+    (
+      biWeekly,
+      "2025-01-01",
+      "2025-12-31",
+      "2025-07-26",
+      "2025-08-01",
+      12,
+      10,
+      Rational(140, 14),
+      347,
+      "working for the whole year with eoy overlap on biweekly pay",
+    ),
+  )
+
+  test(
+    "test tentativeRemainingPayPeriods, remainingPayPeriods, fractionalRemainingPayPeriods, and ordinalEffectiveEndDate",
+  ) {
+    forAll(dataTable) {
+      (
+          frequency,
+          startDateValue,
+          endDateValue,
+          recentPayPeriodEndValue,
+          recentPayDateValue,
+          expectedTentativePayPeriods,
+          expectedTruePayPeriods,
+          expectedFractionalPayPeriods,
+          expectedEffectiveEndDate,
+          description,
+      ) =>
+        When(description)
+        var graph = makeGraphWith(
+          factDictionary,
+          jobs -> Collection(uuidVector),
+          payFrequency -> frequency,
+          startDate -> Day(startDateValue),
+          endDate -> Day(endDateValue),
+          recentPayPeriodEnd -> Day(recentPayPeriodEndValue),
+          recentPayDate -> Day(recentPayDateValue),
+        )
+
+        val tentativePayPeriods = graph.get(tentativeRemainingPayPeriods)
+        val trueRemainingPayPeriods = graph.get(remainingPayPeriods)
+        val fractRemainingPayPeriods = graph.get(fractionalRemainingPayPeriods)
+        val effectiveLastDay = graph.get(ordinalEffectiveEndDate)
+
+        assert(tentativePayPeriods.value.contains(expectedTentativePayPeriods))
+        assert(trueRemainingPayPeriods.value.contains(expectedTruePayPeriods))
+        assert(fractRemainingPayPeriods.value.contains(expectedFractionalPayPeriods))
+        assert(effectiveLastDay.value.contains(expectedEffectiveEndDate))
+
+    }
+  }
+}
