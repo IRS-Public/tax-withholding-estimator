3e3531be82c890473fe640da1fee3a4c92d3d92f
Jared Cunha <1094951+jaredcunha@users.noreply.github.com>
2025-10-14 11:10:28 -0400

    Set up fg sets and collection to work inside containing elements (#791)
    * allow fg sets inside details or other elements
    
    * fix issue with collections inside details
    
    * demo content
    
    * format
    
    * format
    
    * Update the config
    
    * double quotes
    
    ---------
    
    Co-authored-by: Alexander Petros <186603871+petrosgov@users.noreply.github.com>

--- src/main/scala/gov/irs/twe/parser/Section.scala
+++ src/main/scala/gov/irs/twe/parser/Section.scala
@@ -10,35 +10,70 @@ enum SectionNode {
   case rawHTML(node: xml.Node)
 }
 
-case class Section(nodes: List[SectionNode]) {
+case class Section(nodes: List[SectionNode], factDictionary: FactDictionary) {
   def html(templateEngine: TweTemplateEngine): String = {
     val sectionHtml = this.nodes
       .map {
         case SectionNode.fgCollection(x)  => x.html(templateEngine)
         case SectionNode.fgSet(x)         => x.html(templateEngine)
         case SectionNode.fgSectionGate(x) => x.html(templateEngine)
-        case SectionNode.rawHTML(x)       => x
+        case SectionNode.rawHTML(x)       => renderNode(x, templateEngine)
       }
       .mkString("\n")
 
-    "<section>" + sectionHtml + "</section>"
+    "<section class=\"flow\">" + sectionHtml + "</section>"
   }
+
+  private def renderNode(node: xml.Node, templateEngine: TweTemplateEngine): String =
+    // Check if this node or its descendants contain special elements
+    if (containsSpecialElements(node)) {
+      // Process children and reconstruct the node
+      val processedChildren = (node \ "_").map { child =>
+        child.label match {
+          case "fg-collection"   => SectionNode.fgCollection(FgCollection.parse(child, factDictionary))
+          case "fg-set"          => SectionNode.fgSet(FgSet.parse(child, factDictionary))
+          case "fg-section-gate" => SectionNode.fgSectionGate(FgSectionGate.parse(child))
+          case _                 => SectionNode.rawHTML(child)
+        }
+      }
+
+      val childrenHtml = processedChildren.map {
+        case SectionNode.fgCollection(x)  => x.html(templateEngine)
+        case SectionNode.fgSet(x)         => x.html(templateEngine)
+        case SectionNode.fgSectionGate(x) => x.html(templateEngine)
+        case SectionNode.rawHTML(x)       => renderNode(x, templateEngine)
+      }.mkString
+
+      // Reconstruct the node with processed children
+      val attributes = node.attributes.asAttrMap
+        .map { case (k, v) => s"""$k="$v"""" }
+        .mkString(" ")
+      val attrString = if (attributes.nonEmpty) s" $attributes" else ""
+
+      s"<${node.label}$attrString>$childrenHtml</${node.label}>"
+    } else {
+      // No special elements, return as-is
+      node.toString
+    }
+
+  private def containsSpecialElements(node: xml.Node): Boolean =
+    (node \\ "_").exists(n => n.label == "fg-collection" || n.label == "fg-set" || n.label == "fg-section-gate")
 }
 
 object Section {
   def parse(section: xml.Node, factDictionary: FactDictionary): Section = {
     val nodes = (section \ "_")
-      .map(node =>
-        node.label match {
-          case "fg-collection"   => SectionNode.fgCollection(FgCollection.parse(node, factDictionary))
-          case "fg-set"          => SectionNode.fgSet(FgSet.parse(node, factDictionary))
-          case "fg-section-gate" => SectionNode.fgSectionGate(FgSectionGate.parse(node))
-          case _                 => SectionNode.rawHTML(node)
-        },
-      )
+      .map(node => processNode(node, factDictionary))
       .toList
 
-    Section(nodes)
+    Section(nodes, factDictionary)
   }
 
+  private def processNode(node: xml.Node, factDictionary: FactDictionary): SectionNode =
+    node.label match {
+      case "fg-collection"   => SectionNode.fgCollection(FgCollection.parse(node, factDictionary))
+      case "fg-set"          => SectionNode.fgSet(FgSet.parse(node, factDictionary))
+      case "fg-section-gate" => SectionNode.fgSectionGate(FgSectionGate.parse(node))
+      case _                 => SectionNode.rawHTML(node)
+    }
 }
