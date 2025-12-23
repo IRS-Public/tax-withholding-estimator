0d4047c1380cdbaa1de7f8eaacce6e922be1686e
Ronak <84742904+ronaktruss@users.noreply.github.com>
2025-12-23 15:43:16 -0500

    498: Enhance calculation for w4 delay (#1024)

---
+++ src/test/scala/gov/irs/twe/factDictionary/WithholdingCalculationsSpec.scala
@@ -0,0 +1,69 @@
+package gov.irs.twe.factDictionary
+
+import gov.irs.factgraph.types.Collection
+import gov.irs.factgraph.types.Day
+import gov.irs.factgraph.types.Enum
+import gov.irs.factgraph.FactDictionaryForTests
+import gov.irs.factgraph.Path
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+class WithholdingCalculationsSpec extends AnyFunSuite with TableDrivenPropertyChecks {
+  val factDictionary = setupFactDictionary()
+
+  val jobs = Path("/jobs")
+  val jobId = "14e10d4f-728b-496b-aaf9-41c627cf48bf"
+  val jobsCollection = Collection(Vector(java.util.UUID.fromString(jobId)))
+
+  val weekly = Enum("weekly", "/payFrequencyOptions");
+  val biWeekly = Enum("biWeekly", "/payFrequencyOptions");
+  val semiMonthly = Enum("semiMonthly", "/payFrequencyOptions");
+  val monthly = Enum("monthly", "/payFrequencyOptions");
+
+  val dataTable = Table(
+    ("frequency", "runDate", "mostRecentPayDate", "payPeriodDelay"),
+    (weekly, "2025-05-08", "2025-05-02", 3),
+    (weekly, "2025-05-08", "2025-05-05", 2),
+    (weekly, "2025-05-08", "2025-05-08", 2),
+    (biWeekly, "2025-05-17", "2025-05-05", 1),
+    (biWeekly, "2025-05-18", "2025-05-05", 2),
+    (biWeekly, "2025-05-19", "2025-05-19", 1),
+    (semiMonthly, "2025-01-16", "2025-01-16", 1),
+    (semiMonthly, "2025-01-16", "2025-01-14", 1),
+    (semiMonthly, "2025-01-15", "2025-01-14", 1),
+    (semiMonthly, "2025-05-30", "2025-05-20", 1),
+    (semiMonthly, "2025-01-31", "2025-01-30", 1),
+    (semiMonthly, "2025-02-13", "2025-02-07", 1),
+    (semiMonthly, "2025-02-12", "2025-02-07", 1),
+    (semiMonthly, "2025-02-17", "2025-02-10", 1),
+    (semiMonthly, "2025-02-20", "2025-02-17", 1),
+    // We are intentionally ignoring these edge cases because the results are due to stacking assumptions that may not be accurate and add complexity
+    // (semiMonthly, "2025-01-31", "2025-01-31", 0),
+    // (semiMonthly, "2025-01-30", "2025-01-30", 0),
+    (semiMonthly, "2025-02-13", "2025-01-31", 2),
+    (semiMonthly, "2025-02-14", "2025-02-01", 2),
+    (semiMonthly, "2025-02-14", "2025-02-01", 2),
+    (monthly, "2025-05-20", "2025-05-05", 0),
+    (monthly, "2025-05-21", "2025-05-05", 1),
+    (monthly, "2025-06-04", "2025-05-05", 1),
+    (monthly, "2025-02-12", "2025-01-31", 0),
+    (monthly, "2025-02-13", "2025-01-31", 1),
+  )
+
+  test("payPeriodsBeforeW4ChangesAppear varies based on when pay dates and run date") {
+    forAll(dataTable) { (payFreq, runDate, mostRecentPayDate, payPeriodDelay) =>
+      val graph = makeGraphWith(
+        factDictionary,
+        jobs -> jobsCollection,
+        Path(s"/jobs/#$jobId/payFrequency") -> payFreq,
+        Path("/overrideDate") -> Day(runDate),
+        Path(s"/jobs/#$jobId/mostRecentPayDate") -> Day(mostRecentPayDate),
+      )
+      // Sanity check the test inputs
+      assert(Day(mostRecentPayDate).<=(Day(runDate)))
+
+      val payPeriodsBeforeW4ChangesAppear = graph.get(Path(s"/jobs/#${jobId}/payPeriodsBeforeW4ChangesAppear"))
+      assert(payPeriodsBeforeW4ChangesAppear.value.contains(payPeriodDelay))
+    }
+  }
+}
