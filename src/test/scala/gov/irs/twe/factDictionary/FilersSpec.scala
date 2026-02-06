f316b59d31c61d0f2e4f2cbad3d10c6357a677f8
Brandon Lenz <15805554+brandonlenz@users.noreply.github.com>
2026-02-05 20:07:11 -0500

    Fix general Credit Eligibility for non-mfj filng status (#1545)

--- src/test/scala/gov/irs/twe/factDictionary/FilersSpec.scala
+++ src/test/scala/gov/irs/twe/factDictionary/FilersSpec.scala
@@ -14,25 +14,58 @@ class FilersSpec extends AnyFunSuite with TableDrivenPropertyChecks {
         "primaryFilerIsBlind",
         // The value to insert into the fact graph. "None" will skip insertion resulting in an incomplete writable fact
         "secondaryFilerIsBlind",
-        // expected value. "None" means that we expect the fact to be incomplete
         "expectedOnlyPrimaryFilerIsBlind",
-        // expected value. "None" means that we expect the fact to be incomplete
         "expectedOnlySecondaryFilerIsBlind",
-        // expected value. "None" means that we expect the fact to be incomplete
         "expectedPrimaryAndSecondaryFilersAreBlind",
       ),
-      (Some(true), None, Some(true), Some(false), None),
-      (Some(true), Some(false), Some(true), Some(false), Some(false)),
-      (None, Some(true), Some(false), Some(true), None),
-      (Some(false), Some(true), Some(false), Some(true), Some(false)),
-      (Some(true), Some(true), Some(false), Some(false), Some(true)),
-      (Some(false), Some(false), Some(false), Some(false), Some(false)),
+      (
+        Some(true),
+        None,
+        true,
+        false,
+        false,
+      ),
+      (
+        Some(true),
+        Some(false),
+        true,
+        false,
+        false,
+      ),
+      (
+        None,
+        Some(true),
+        false,
+        true,
+        false,
+      ),
+      (
+        Some(false),
+        Some(true),
+        false,
+        true,
+        false,
+      ),
+      (
+        Some(true),
+        Some(true),
+        false,
+        false,
+        true,
+      ),
+      (
+        Some(false),
+        Some(false),
+        false,
+        false,
+        false,
+      ),
       (
         None,
         None,
-        None,
-        None,
-        None,
+        false,
+        false,
+        false,
       ),
     )
 
@@ -52,27 +85,14 @@ class FilersSpec extends AnyFunSuite with TableDrivenPropertyChecks {
         val actualOnlySecondaryFilerIsBlind = graph.get("/onlySecondaryFilerIsBlind")
         val actualPrimaryAndSecondaryFilersAreBlind = graph.get("/primaryAndSecondaryFilersAreBlind")
 
-        if (expectedOnlyPrimaryFilerIsBlind.isEmpty)
-          assert(!actualOnlyPrimaryFilerIsBlind.complete)
-          assert(actualOnlyPrimaryFilerIsBlind.value.isEmpty)
-        else {
-          assert(actualOnlyPrimaryFilerIsBlind.complete)
-          assert(actualOnlyPrimaryFilerIsBlind.value.contains(expectedOnlyPrimaryFilerIsBlind.get))
-        }
+        assert(actualOnlyPrimaryFilerIsBlind.complete)
+        assert(actualOnlyPrimaryFilerIsBlind.value.contains(expectedOnlyPrimaryFilerIsBlind))
 
-        if (expectedOnlySecondaryFilerIsBlind.isEmpty)
-          assert(!actualOnlySecondaryFilerIsBlind.complete)
-          assert(actualOnlySecondaryFilerIsBlind.value.isEmpty)
-        else
-          assert(actualOnlySecondaryFilerIsBlind.complete)
-          assert(actualOnlySecondaryFilerIsBlind.value.contains(expectedOnlySecondaryFilerIsBlind.get))
+        assert(actualOnlySecondaryFilerIsBlind.complete)
+        assert(actualOnlySecondaryFilerIsBlind.value.contains(expectedOnlySecondaryFilerIsBlind))
 
-        if (expectedPrimaryAndSecondaryFilersAreBlind.isEmpty)
-          assert(!actualPrimaryAndSecondaryFilersAreBlind.complete)
-          assert(actualPrimaryAndSecondaryFilersAreBlind.value.isEmpty)
-        else
-          assert(actualPrimaryAndSecondaryFilersAreBlind.complete)
-          assert(actualPrimaryAndSecondaryFilersAreBlind.value.contains(expectedPrimaryAndSecondaryFilersAreBlind.get))
+        assert(actualPrimaryAndSecondaryFilersAreBlind.complete)
+        assert(actualPrimaryAndSecondaryFilersAreBlind.value.contains(expectedPrimaryAndSecondaryFilersAreBlind))
     }
   }
 
@@ -83,25 +103,66 @@ class FilersSpec extends AnyFunSuite with TableDrivenPropertyChecks {
         "primaryFilerIsClaimedOnAnotherReturn",
         // The value to insert into the fact graph. "None" will skip insertion resulting in an incomplete writable fact
         "secondaryFilerIsClaimedOnAnotherReturn",
-        // expected value. "None" means that we expect the fact to be incomplete
         "expectedOnlyPrimaryFilerIsClaimedOnAnotherReturn",
-        // expected value. "None" means that we expect the fact to be incomplete
         "expectedOnlySecondaryFilerIsClaimedOnAnotherReturn",
-        // expected value. "None" means that we expect the fact to be incomplete
         "expectedPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn",
+        "expectedNeitherFilerIsClaimedOnAnotherReturn",
       ),
-      (Some(true), None, Some(true), Some(false), None),
-      (Some(true), Some(false), Some(true), Some(false), Some(false)),
-      (None, Some(true), Some(false), Some(true), None),
-      (Some(false), Some(true), Some(false), Some(true), Some(false)),
-      (Some(true), Some(true), Some(false), Some(false), Some(true)),
-      (Some(false), Some(false), Some(false), Some(false), Some(false)),
+      (
+        Some(true),
+        None,
+        true,
+        false,
+        false,
+        false,
+      ),
+      (
+        Some(true),
+        Some(false),
+        true,
+        false,
+        false,
+        false,
+      ),
+      (
+        None,
+        Some(true),
+        false,
+        true,
+        false,
+        false,
+      ),
+      (
+        Some(false),
+        Some(true),
+        false,
+        true,
+        false,
+        false,
+      ),
+      (
+        Some(true),
+        Some(true),
+        false,
+        false,
+        true,
+        false,
+      ),
+      (
+        Some(false),
+        Some(false),
+        false,
+        false,
+        false,
+        true,
+      ),
       (
         None,
         None,
-        None,
-        None,
-        None,
+        false,
+        false,
+        false,
+        true,
       ),
     )
 
@@ -112,6 +173,7 @@ class FilersSpec extends AnyFunSuite with TableDrivenPropertyChecks {
           expectedOnlyPrimaryFilerIsClaimedOnAnotherReturn,
           expectedOnlySecondaryFilerIsClaimedOnAnotherReturn,
           expectedPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn,
+          expectedNeitherFilerIsClaimedOnAnotherReturn,
       ) =>
         val graph = makeGraphWith(factDictionary)
         primaryFilerIsClaimedOnAnotherReturn.foreach(it => graph.set(Path("/primaryFilerIsClaimedOnAnotherReturn"), it))
@@ -123,40 +185,35 @@ class FilersSpec extends AnyFunSuite with TableDrivenPropertyChecks {
         val actualOnlySecondaryFilerIsClaimedOnAnotherReturn = graph.get("/onlySecondaryFilerIsClaimedOnAnotherReturn")
         val actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn =
           graph.get("/primaryAndSecondaryFilersAreClaimedOnAnotherReturn")
+        val actualNeitherFilerIsClaimedOnAnotherReturn = graph.get("/neitherFilerIsClaimedOnAnotherReturn")
 
-        if (expectedOnlyPrimaryFilerIsClaimedOnAnotherReturn.isEmpty)
-          assert(!actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.complete)
-          assert(actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.value.isEmpty)
-        else {
-          assert(actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.complete)
-          assert(
-            actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.value.contains(
-              expectedOnlyPrimaryFilerIsClaimedOnAnotherReturn.get,
-            ),
-          )
-        }
+        assert(actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.complete)
+        assert(
+          actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.value.contains(
+            expectedOnlyPrimaryFilerIsClaimedOnAnotherReturn,
+          ),
+        )
 
-        if (expectedOnlySecondaryFilerIsClaimedOnAnotherReturn.isEmpty)
-          assert(!actualOnlySecondaryFilerIsClaimedOnAnotherReturn.complete)
-          assert(actualOnlySecondaryFilerIsClaimedOnAnotherReturn.value.isEmpty)
-        else
-          assert(actualOnlySecondaryFilerIsClaimedOnAnotherReturn.complete)
-          assert(
-            actualOnlySecondaryFilerIsClaimedOnAnotherReturn.value.contains(
-              expectedOnlySecondaryFilerIsClaimedOnAnotherReturn.get,
-            ),
-          )
+        assert(actualOnlySecondaryFilerIsClaimedOnAnotherReturn.complete)
+        assert(
+          actualOnlySecondaryFilerIsClaimedOnAnotherReturn.value.contains(
+            expectedOnlySecondaryFilerIsClaimedOnAnotherReturn,
+          ),
+        )
 
-        if (expectedPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.isEmpty)
-          assert(!actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.complete)
-          assert(actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.value.isEmpty)
-        else
-          assert(actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.complete)
-          assert(
-            actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.value.contains(
-              expectedPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.get,
-            ),
-          )
+        assert(actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.complete)
+        assert(
+          actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.value.contains(
+            expectedPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn,
+          ),
+        )
+
+        assert(actualNeitherFilerIsClaimedOnAnotherReturn.complete)
+        assert(
+          actualNeitherFilerIsClaimedOnAnotherReturn.value.contains(
+            expectedNeitherFilerIsClaimedOnAnotherReturn,
+          ),
+        )
     }
   }
 
@@ -167,25 +224,58 @@ class FilersSpec extends AnyFunSuite with TableDrivenPropertyChecks {
         "primaryFilerAge65OrOlder",
         // The value to insert into the fact graph. "None" will skip insertion resulting in an incomplete writable fact
         "secondaryFilerAge65OrOlder",
-        // expected value. "None" means that we expect the fact to be incomplete
         "expectedOnlyPrimaryFilerAge65OrOlder",
-        // expected value. "None" means that we expect the fact to be incomplete
         "expectedOnlySecondaryFilerAge65OrOlder",
-        // expected value. "None" means that we expect the fact to be incomplete
         "expectedPrimaryAndSecondaryFilersAge65OrOlder",
       ),
-      (Some(true), None, Some(true), Some(false), None),
-      (Some(true), Some(false), Some(true), Some(false), Some(false)),
-      (None, Some(true), Some(false), Some(true), None),
-      (Some(false), Some(true), Some(false), Some(true), Some(false)),
-      (Some(true), Some(true), Some(false), Some(false), Some(true)),
-      (Some(false), Some(false), Some(false), Some(false), Some(false)),
+      (
+        Some(true),
+        None,
+        true,
+        false,
+        false,
+      ),
+      (
+        Some(true),
+        Some(false),
+        true,
+        false,
+        false,
+      ),
+      (
+        None,
+        Some(true),
+        false,
+        true,
+        false,
+      ),
+      (
+        Some(false),
+        Some(true),
+        false,
+        true,
+        false,
+      ),
+      (
+        Some(true),
+        Some(true),
+        false,
+        false,
+        true,
+      ),
+      (
+        Some(false),
+        Some(false),
+        false,
+        false,
+        false,
+      ),
       (
         None,
         None,
-        None,
-        None,
-        None,
+        false,
+        false,
+        false,
       ),
     )
 
@@ -201,40 +291,27 @@ class FilersSpec extends AnyFunSuite with TableDrivenPropertyChecks {
         primaryFilerAge65OrOlder.foreach(it => graph.set(Path("/primaryFilerAge65OrOlder"), it))
         secondaryFilerAge65OrOlder.foreach(it => graph.set(Path("/secondaryFilerAge65OrOlder"), it))
 
-        val actualOnlyPrimaryFilerIsClaimedOnAnotherReturn = graph.get("/onlyPrimaryFilerAge65OrOlder")
-        val actualOnlySecondaryFilerIsClaimedOnAnotherReturn = graph.get("/onlySecondaryFilerAge65OrOlder")
-        val actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn =
+        val actualOnlyPrimaryFilerAge65OrOlder = graph.get("/onlyPrimaryFilerAge65OrOlder")
+        val actualOnlySecondaryFilerAge65OrOlder = graph.get("/onlySecondaryFilerAge65OrOlder")
+        val actualPrimaryAndSecondaryFilersAge65OrOlder =
           graph.get("/primaryAndSecondaryFilersAge65OrOlder")
 
-        if (expectedOnlyPrimaryFilerAge65OrOlder.isEmpty)
-          assert(!actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.complete)
-          assert(actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.value.isEmpty)
-        else {
-          assert(actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.complete)
-          assert(
-            actualOnlyPrimaryFilerIsClaimedOnAnotherReturn.value.contains(expectedOnlyPrimaryFilerAge65OrOlder.get),
-          )
-        }
+        assert(actualOnlyPrimaryFilerAge65OrOlder.complete)
+        assert(
+          actualOnlyPrimaryFilerAge65OrOlder.value.contains(expectedOnlyPrimaryFilerAge65OrOlder),
+        )
 
-        if (expectedOnlySecondaryFilerAge65OrOlder.isEmpty)
-          assert(!actualOnlySecondaryFilerIsClaimedOnAnotherReturn.complete)
-          assert(actualOnlySecondaryFilerIsClaimedOnAnotherReturn.value.isEmpty)
-        else
-          assert(actualOnlySecondaryFilerIsClaimedOnAnotherReturn.complete)
-          assert(
-            actualOnlySecondaryFilerIsClaimedOnAnotherReturn.value.contains(expectedOnlySecondaryFilerAge65OrOlder.get),
-          )
+        assert(actualOnlySecondaryFilerAge65OrOlder.complete)
+        assert(
+          actualOnlySecondaryFilerAge65OrOlder.value.contains(expectedOnlySecondaryFilerAge65OrOlder),
+        )
 
-        if (expectedPrimaryAndSecondaryFilersAge65OrOlder.isEmpty)
-          assert(!actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.complete)
-          assert(actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.value.isEmpty)
-        else
-          assert(actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.complete)
-          assert(
-            actualPrimaryAndSecondaryFilersAreClaimedOnAnotherReturn.value.contains(
-              expectedPrimaryAndSecondaryFilersAge65OrOlder.get,
-            ),
-          )
+        assert(actualPrimaryAndSecondaryFilersAge65OrOlder.complete)
+        assert(
+          actualPrimaryAndSecondaryFilersAge65OrOlder.value.contains(
+            expectedPrimaryAndSecondaryFilersAge65OrOlder,
+          ),
+        )
     }
   }
 }
