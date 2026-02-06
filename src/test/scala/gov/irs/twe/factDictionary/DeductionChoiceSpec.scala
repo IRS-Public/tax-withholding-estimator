593526dec7f6ed9479d0378d96332ff52e7ec41d
Alexander Petros <186603871+petrosgov@users.noreply.github.com>
2026-02-06 16:14:05 -0500

    Require a selection for standard deduction (#1573)

--- src/test/scala/gov/irs/twe/factDictionary/DeductionChoiceSpec.scala
+++ src/test/scala/gov/irs/twe/factDictionary/DeductionChoiceSpec.scala
@@ -6,23 +6,6 @@ import org.scalatest.funsuite.AnyFunSuite
 class DeductionChoiceSpec extends AnyFunSuite {
   val factDictionary = setupFactDictionary()
 
-  test("Deduction preference is properly reflected when the Taxpayer has not yet made a choice") {
-    // given: the Taxpayer has not yet made a deduction choice
-    val graph = makeGraphWith(factDictionary)
-
-    // when: inspecting the deduction choice facts
-    val wantsStandardDeduction = graph.get("/wantsStandardDeduction")
-    val wantsItemizedDeduction = graph.get("/wantsItemizedDeduction")
-
-    // then: /wantsStandardDeduction (writable) is incomplete with a placeholder value ("Pending" state)
-    assert(!wantsStandardDeduction.complete)
-    assert(wantsStandardDeduction.value.get === true)
-
-    // and: /wantsItemizedDeduction (derived) is incomplete but false
-    assert(!wantsItemizedDeduction.complete)
-    assert(wantsItemizedDeduction.value.get == false)
-  }
-
   test("Deduction preference is properly reflected when the Taxpayer prefers the standard deduction") {
     // given: the Taxpayer prefers the standard deduction
     val graph = makeGraphWith(factDictionary)
