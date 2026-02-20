d0b39ae798696637b08a749f981bd4e156bb8fdd
Brandon Lenz <15805554+brandonlenz@users.noreply.github.com>
2026-02-20 13:39:33 -0500

    Display content recommending making estimated tax payments to make up for a withholding gap greater than remaining withholdable income (#1613)

--- src/test/scala/gov/irs/twe/factDictionary/WithholdingCalculationsSpec.scala
+++ src/test/scala/gov/irs/twe/factDictionary/WithholdingCalculationsSpec.scala
@@ -1,27 +1,24 @@
 package gov.irs.twe.factDictionary
 
-import gov.irs.factgraph.types.Collection
-import gov.irs.factgraph.types.Day
-import gov.irs.factgraph.types.Dollar
-import gov.irs.factgraph.types.Enum
-import gov.irs.factgraph.FactDictionaryForTests
+import gov.irs.factgraph.types.{ Collection, CollectionItem, Day, Dollar, Enum }
 import gov.irs.factgraph.Path
+import java.util.UUID
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.prop.TableDrivenPropertyChecks
 
 class WithholdingCalculationsSpec extends AnyFunSuite with TableDrivenPropertyChecks {
-  val factDictionary = setupFactDictionary()
+  private val factDictionary = setupFactDictionary()
 
-  val jobs = Path("/jobs")
-  val jobId = "14e10d4f-728b-496b-aaf9-41c627cf48bf"
-  val jobsCollection = Collection(Vector(java.util.UUID.fromString(jobId)))
+  private val jobs = Path("/jobs")
+  private val jobId = "14e10d4f-728b-496b-aaf9-41c627cf48bf"
+  private val jobsCollection = Collection(Vector(java.util.UUID.fromString(jobId)))
 
-  val weekly = Enum("weekly", "/payFrequencyOptions");
-  val biWeekly = Enum("biWeekly", "/payFrequencyOptions");
-  val semiMonthly = Enum("semiMonthly", "/payFrequencyOptions");
-  val monthly = Enum("monthly", "/payFrequencyOptions");
+  private val weekly = Enum("weekly", "/payFrequencyOptions");
+  private val biWeekly = Enum("biWeekly", "/payFrequencyOptions");
+  private val semiMonthly = Enum("semiMonthly", "/payFrequencyOptions");
+  private val monthly = Enum("monthly", "/payFrequencyOptions");
 
-  val dataTable = Table(
+  private val dataTable = Table(
     ("frequency", "runDate", "mostRecentPayDate", "payPeriodDelay"),
     (weekly, "2025-05-08", "2025-05-05", 2),
     (weekly, "2025-05-08", "2025-05-08", 2),
@@ -85,6 +82,124 @@ class WithholdingCalculationsSpec extends AnyFunSuite with TableDrivenPropertyCh
       assert(
         graph.get(Path(s"/jobs/#$jobId/withholdingsFromTotalBonusReceived")).value.contains(Dollar(expectedWithholding)),
       )
+    }
+  }
+
+  test(
+    "/mayNeedToMakeEstimatedPayments properly indicates whether or not the taxpayer is able to cover their tax balance via extra job withholding alone",
+  ) {
+    val testParameters = Table(
+      (
+        "withholdingAmountPerPayPeriod",
+        "averagePayPerPayPeriodForWithholding",
+        "expectedMayNeedToMakeEstimatedPayments",
+      ),
+      // TRUE - Will need to make estimated payments because recommended withholding exceeds average pay
+      (1000, 999, true),
+
+      // TRUE - Tiebreaker case. Taxpayer earn make enough to bring their balance to 0, however, that's only if they act
+      //        immediately, and if everything they entered is accurate. Since we say "may need to make estimated payments",
+      //        in the case of a tie, show the content anyway to encourage taxpayers to double-check when they might
+      //        be in this scenario.
+      (1000, 1000, true),
+
+      // FALSE - Will not need to make estimated payments because remaining pay is larger than withholding
+      (1000, 1001, false),
+      (1000, 10000, false),
+
+      // FALSE - Will not need to make estimated payments because there is no extra withholding
+      (0, 0, false),
+      (0, 1000, false),
+    )
+
+    forAll(testParameters) {
+      (
+          withholdingAmountPerPayPeriod,
+          averagePayPerPayPeriodForWithholding,
+          expectedMayNeedToMakeEstimatedPayments,
+      ) =>
+        // given
+        val graph = makeGraphWith(
+          factDictionary,
+          Path("/jobSelectedForExtraWithholding") -> CollectionItem(UUID.randomUUID()),
+          Path("/jobSelectedForExtraWithholding/averagePayPerPayPeriodForWithholding") -> Dollar(
+            averagePayPerPayPeriodForWithholding,
+          ),
+          Path("/jobSelectedForExtraWithholding/withholdingAmountPerPayPeriod") -> Dollar(
+            withholdingAmountPerPayPeriod,
+          ),
+        )
+
+        // when
+        val mayNeedToMakeEstimatedPayments = graph.get("/mayNeedToMakeEstimatedPayments")
+
+        // then
+        assert(mayNeedToMakeEstimatedPayments.hasValue)
+        assert(mayNeedToMakeEstimatedPayments.get == expectedMayNeedToMakeEstimatedPayments)
+    }
+  }
+
+  test(
+    "/mayNeedToMakeEstimatedPayments properly indicates whether or not the taxpayer is able to cover their tax balance via extra pension withholding alone",
+  ) {
+    val testParameters = Table(
+      (
+        "withholdingAmountPerPayPeriod",
+        "averagePayPerPayPeriodForWithholding",
+        "expectedMayNeedToMakeEstimatedPayments",
+      ),
+      // TRUE - Will need to make estimated payments because recommended withholding exceeds average pay
+      (1000, 999, true),
+
+      // TRUE - Tiebreaker case. Taxpayer does earn enough to bring their balance to 0, however, that's only if they act
+      //        immediately, and if everything they entered is accurate. Since we say "may need to make estimated payments",
+      //        in the case of a tie, show the content anyway to encourage taxpayers to double-check when they might
+      //        be in this scenario.
+      (1000, 1000, true),
+
+      // FALSE - Will not need to make estimated payments because remaining pay is larger than withholding
+      (1000, 1001, false),
+      (1000, 10000, false),
+
+      // FALSE - Will not need to make estimated payments because there is no extra withholding
+      (0, 0, false),
+      (0, 1000, false),
+    )
+
+    forAll(testParameters) {
+      (
+          withholdingAmountPerPayPeriod,
+          averagePayPerPayPeriodForWithholding,
+          expectedMayNeedToMakeEstimatedPayments,
+      ) =>
+        val pensionId = UUID.randomUUID()
+        val pensions = Collection(Vector(pensionId))
+        val taxYear = 2026
+        val today = Day(s"$taxYear-01-01")
+        // given
+        val graph = makeGraphWith(
+          factDictionary,
+          Path("/pensions") -> pensions,
+          Path(s"/pensions/#$pensionId/isAllYear") -> true,
+          Path(s"/pensions/#$pensionId/averagePayPerPayPeriodForWithholding") -> Dollar(
+            averagePayPerPayPeriodForWithholding,
+          ),
+          Path(s"/pensions/#$pensionId/withholdingAmountPerPayPeriod") -> Dollar(
+            withholdingAmountPerPayPeriod,
+          ),
+          Path(s"/pensions/#$pensionId/yearToDateIncome") -> Dollar(averagePayPerPayPeriodForWithholding),
+          Path(s"/pensions/#$pensionId/yearToDateWithholding") -> Dollar(0),
+          Path(s"/pensions/#$pensionId/payFrequency") -> Enum("monthly", "/payFrequencyOptions"),
+          Path(s"/pensions/#$pensionId/mostRecentPayDate") -> today,
+          Path("/today") -> today,
+        )
+
+        // when
+        val mayNeedToMakeEstimatedPayments = graph.get("/mayNeedToMakeEstimatedPayments")
+
+        // then
+        assert(mayNeedToMakeEstimatedPayments.hasValue)
+        assert(mayNeedToMakeEstimatedPayments.get == expectedMayNeedToMakeEstimatedPayments)
     }
   }
 }
