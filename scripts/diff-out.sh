e9ebecaa3131e27354eb744c1927f39b55d00c5e
Brandon Lenz <15805554+brandonlenz@users.noreply.github.com>
2026-04-10 15:11:54 -0400

    `diff-out` script improvements (#1903)

--- scripts/diff-out.sh
+++ scripts/diff-out.sh
@@ -8,17 +8,25 @@
 set -euo pipefail
 
 BASE_BRANCH="main"
+CURRENT_BRANCH="$(git branch --show-current)"
 WORKTREE_DIR="/tmp/$BASE_BRANCH-branch"
 LOGS_PREFIX="[$(basename "$0")]:"
 
-if [ "$(git branch --show-current)" = "$BASE_BRANCH" ]; then
-  echo "$LOGS_PREFIX Already on main, nothing to compare."
+cleanup() {
+  echo "$LOGS_PREFIX Cleaning up..."
+  git worktree remove $WORKTREE_DIR 2>/dev/null || true
+  echo "$LOGS_PREFIX Done."
+}
+
+if [ "$CURRENT_BRANCH" = "$BASE_BRANCH" ]; then
+  echo "$LOGS_PREFIX Already on \`$BASE_BRANCH\`, nothing to compare."
   exit 0
 fi
+echo "$LOGS_PREFIX Diffing \`$CURRENT_BRANCH\` against \`$BASE_BRANCH\`."
 
-echo "$LOGS_PREFIX Checking out clean '$BASE_BRANCH' worktree..."
-git worktree remove $WORKTREE_DIR 2>/dev/null || true
+echo "$LOGS_PREFIX Checking out clean \`$BASE_BRANCH\` worktree..."
 git worktree add $WORKTREE_DIR main
+trap cleanup EXIT
 
 echo "$LOGS_PREFIX Building twe on \`$BASE_BRANCH\` worktree..."
 (cd $WORKTREE_DIR && make twe)
@@ -26,9 +34,8 @@ echo "$LOGS_PREFIX Building twe on \`$BASE_BRANCH\` worktree..."
 echo "$LOGS_PREFIX Building twe on working copy..."
 make twe
 
-echo "$LOGS_PREFIX Diffing static assets..."
-git diff --no-index --color=always $WORKTREE_DIR/out out | cat || true
-
-echo "$LOGS_PREFIX Cleaning up..."
-git worktree remove $WORKTREE_DIR
-echo "$LOGS_PREFIX Diff complete."
+echo "$LOGS_PREFIX Diffing build outputs..."
+git diff --no-index --color=always $WORKTREE_DIR/out out | cat
+if [ "${PIPESTATUS[0]}" -eq 0 ]; then
+  echo "$LOGS_PREFIX No diff found. Build outputs on \`$CURRENT_BRANCH\` are equivalent with \`$BASE_BRANCH\`."
+fi
